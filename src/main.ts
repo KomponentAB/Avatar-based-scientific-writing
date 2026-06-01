c2f45974cf09dad1016c8b5300de050de0b0ddec
Kili S <79143155+klinshy@users.noreply.github.com>
2026-06-01 18:29:07 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
-
+console.log(
+  "⚠️main.ts loaded, however moduleX-main.ts should have been loaded",
+);
 WA.onInit().then(async () => {
     console.log('loading main.ts');
     try {
